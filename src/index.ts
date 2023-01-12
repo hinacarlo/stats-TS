65124e6b7907bbaf411ef9bb7ad201e9592d59bc
hinacarlo <hinacarlo1993@gmail.com>
2023-01-12 22:54:17 +0800

    Refactor to have a composition based approach between classes

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
+import { CSVFileReader } from './CSVFileReader';
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
+
 // enum - enumeration that stored closely related values - TS
 // purpose is for other engineer. it's like an object
 // but it's for other engineers to read that it is a collection of closely related values
 // only use enums when you know before hand the all possible values  when writing the code
 
 let manUnitedWins = 0;
-const reader = new MatchReader('football.csv');
-reader.read();
+const csvFileReader = new CSVFileReader('football.csv');
+const reader = new MatchReader(csvFileReader);
+reader.load();
 
-console.log(reader.data[0]);
+console.log(reader.matches[0]);
 
-for (let match of reader.data) {
+for (let match of reader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
--- src/inheritance/CSVFileReader.ts
+++ src/inheritance/CSVFileReader.ts
@@ -1,10 +1,11 @@
 import fs from 'fs';
-import { MatchReader } from './MatchReader';
 
-export class CSVFileReader {
-  data: string[][] = [];
+export abstract class CSVFileReader<T> {
+  data: T[] = [];
 
   constructor(public fileName: string) {}
+
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
@@ -14,6 +15,7 @@ export class CSVFileReader {
       .split('\n')
       .map((row: string): string[] => {
         return row.split(',');
-      });
+      })
+      .map(this.mapRow);
   }
 }
